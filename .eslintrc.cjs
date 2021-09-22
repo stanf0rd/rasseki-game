fb1f7325d05586286a2b9b9e96693df88cf51a9a
Dmitry Paliy <dmitrydpaliy@gmail.com>
2021-09-23 01:06:23 +0300

    build(styles): scss installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
     'svelte3/typescript': () => require('typescript'),
+    'svelte3/ignore-styles': () => true,
   },
   parserOptions: {
     sourceType: 'module',

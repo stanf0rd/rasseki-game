cf35d8ce125add5998b1e31c6f2f5d1f48f213b9
Dmitry Paliy <dmitrydpaliy@gmail.com>
2021-12-12 22:04:52 +0300

    feat(engine): created GameMap class that fetches and stores map data

---
+++ src/core/gamemap.ts
@@ -0,0 +1,42 @@
+/** Class that fetches and stores map data and provides access to it */
+export default class GameMap {
+    private source: string;
+    private map: Uint8Array;
+    private width: number;
+    private height: number;
+
+    constructor() {
+        this.source = '/maps/1/surfaces.rsk';
+        this.width = 160;
+        this.height = 144;
+
+        fetch(this.source)
+            .then(async response => {
+                const reader = response.body.getReader();
+
+                let { done, value } = await reader.read();
+                while (!done) {
+                    if (!this.map) this.map = value;
+                    else this.map.set(value, this.map.length);
+
+                    console.log(`Current chunk length is ${value.length}`);
+                    ({ done, value } = await reader.read());
+                }
+            })
+            .catch(console.error);
+    }
+
+    public getSurface(x: number, y: number): number {
+        return this.map[this.width * y + x];
+    }
+}
+
+/**
+ * 10x10
+ * 0 0 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0 0 0
+ */

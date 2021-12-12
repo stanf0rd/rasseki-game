cd5c0b8fe8c0c5ce712491b812ebd3b08ee437f4
Dmitry Paliy <dmitrydpaliy@gmail.com>
2021-12-12 21:57:37 +0300

    feat(surface map generator): add generator of rsk-file with surface layout info

---
+++ scripts/readtiff.ts
@@ -0,0 +1,51 @@
+import { readFile, writeFile } from 'fs/promises';
+
+import type TiffIfd from 'tiff/lib/tiffIfd';
+import type { DataArray } from 'tiff/lib/types';
+
+/* UTILITY */
+function getPixelColor(data: DataArray, index: number): number {
+    const r = data[4 * index],
+        g = data[4 * index + 1],
+        b = data[4 * index + 2],
+        a = data[4 * index + 3];
+
+    return 1000000 * r + 1000 * g + b;
+}
+
+function getSurfaces(page: TiffIfd): Record<number, number> {
+    const colors: Record<number, number> = {};
+
+    for (let i = 0; i < page.width; i++) {
+        const color = getPixelColor(page.data, i);
+        if (color in colors) break;
+        colors[color] = i;
+    }
+
+    return colors;
+}
+
+async function main() {
+    const tiff = await import('tiff');
+    const file = await readFile('scripts/map.tiff');
+
+    const [page] = tiff.decode(file);
+    const { width, height } = page;
+
+    const surfaces = getSurfaces(page);
+    const buffer = Buffer.alloc(page.size);
+    for (let line = 0; line < height; line++) {
+        for (let i = 0; i < width; i++) {
+            const color = getPixelColor(page.data, line * width + i);
+
+            const colorID = surfaces[color];
+            if (colorID) {
+                buffer.writeUInt8(colorID, line * width + i);
+            }
+        }
+    }
+
+    await writeFile('image.rsk', buffer);
+}
+
+main();
